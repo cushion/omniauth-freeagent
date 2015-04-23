1acc4044a50ec4657e2380182586f78f197ae99f
Jonnie Hallman <jonnie@destroytoday.com>
2015-04-23 15:31:37 -0400

    Added company info to extra

--- lib/omniauth/strategies/freeagent.rb
+++ lib/omniauth/strategies/freeagent.rb
@@ -4,7 +4,7 @@ module OmniAuth
   module Strategies
     class FreeAgent < OmniAuth::Strategies::OAuth2
       option :name, 'freeagent'
-      
+
       option :client_options, {
         :site => 'https://api.freeagent.com',
         :authorize_url => '/v2/approve_app',
@@ -31,7 +31,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/v2/users/me').parsed['user']
+        @raw_info ||= access_token.get('/v2/users/me').parsed.merge(access_token.get('/v2/company').parsed)
       end
     end
   end
